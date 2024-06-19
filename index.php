6a20c5072cf14253c612adfd20622384af3d2351
Yosshiii43 <daisy440440@gmail.com>
2024-06-20 03:24:44 +0900

    index.phpを初期状態へ

--- index.php
+++ index.php
@@ -1,1 +1,134 @@
-<p>テストテスト</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@100;300;400;500;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">    <link rel="stylesheet" href="./css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script type="text/javascript" src="./js/main.js"></script>
+
+</head>
+<body class="p-index c-body">
+    <div class="wrapper">
+    <header class="l-header p-header">
+        <button class="p-header__btn c-title js-navButton"><span>Menu</span></button>
+        <p class="p-header__ttl c-title"><a href="#">Hamburger</a></p>
+        <div class="p-header__search">
+            <form action="" >
+                <div class="c-searchBox">
+                    <input type="search">
+                    <p class="c-seachIcon"><img src="./img/search__icon.png" alt="ルーペアイコン" width="24px" height="24px"></p>
+                </div>
+                <button class="c-button" type="button" >検索</button>
+            </form>
+        </div>
+    </header>
+    <main class="l-main">
+        <div class="p-mainVisual--front">
+            <div class="p-pageTitle--front">
+                <h1>ダミーサイト</h1>
+            </div>
+        </div>
+        <div class="p-wrapper p-wrapper--front">
+            <article class="p-salesFormat__wrap">
+                <article class="p-salesFormat">
+                    <a href="#">
+                        <div class="p-salesFormat__ttl">
+                            <h2 class="c-title">Take Out</h2>
+                            <div class="c-horizon"></div>
+                        </div>
+                        <div class="p-salesFormat__content">
+                            <dl class="p-salesItem">
+                                <dt class="p-salesItem__ttl c-title c-font-mplus">Take Out</dt>
+                                <dd class="p-salesItem__txt c-text">当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています</dd>
+                            </dl>
+                            <dl class="p-salesItem">
+                                <dt class="p-salesItem__ttl c-title c-font-mplus">Take Out</dt>
+                                <dd class="p-salesItem__txt c-text">当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています</dd>
+                            </dl>
+                        </div>
+                    </a>
+                </article>
+                <article class="p-salesFormat">
+                    <a href="#">
+                        <div class="p-salesFormat__ttl">
+                            <h2 class="c-title ">Eat In</h2>
+                            <div class="c-horizon"></div>
+                        </div>
+                        <div class="p-salesFormat__content">
+                            <dl class="p-salesItem">
+                                <dt class="p-salesItem__ttl c-title c-font-mplus">Eat In</dt>
+                                <dd class="p-salesItem__txt c-text">店内でお食事いただけるメニューです店内でお食事いただけるメニューです店内でお食事いただけるメニューです</dd>
+                            </dl>
+                            <dl class="p-salesItem">
+                                <dt class="p-salesItem__ttl c-title c-font-mplus">Eat In</dt>
+                                <dd class="p-salesItem__txt c-text">店内でお食事いただけるメニューです店内でお食事いただけるメニューです店内でお食事いただけるメニューです</dd>
+                            </dl>
+                        </div>
+                    </a>
+                </article>
+            </article>
+        </div>
+        <div class="p-access">
+                <article class="p-access__caption">
+                    <div>
+                    <h2 class="p-access__caption__ttl c-title">見出しが入ります</h2>
+                    <div class="p-access__caption__horizon c-horizon"></div>
+                    <p class="p-access__caption__txt c-lineHeightWide c-text-smal">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入りま</p>
+                    </div>
+                </article>
+                <div class="p-access__base"></div>
+        </div>
+    </main>
+    <aside class="l-sidebar p-sidebar">
+        <div class="p-navbg"></div>
+        <nav class="p-nav">
+            <h2 class="c-title">Menu</h2>
+            <ul class="p-gnav c-gnav">
+                <li ><a href="#">バーガー</a>
+                    <ul class="c-gnav">
+                        <li><a href="#">ハンバーガー</a></li>
+                        <li><a href="#">チーズバーガー</a></li>
+                        <li><a href="#">テリヤキバーガー</a></li>
+                        <li><a href="#">アボガドバーガー</a></li>
+                        <li><a href="#">フィッシュバーガー</a></li>
+                        <li><a href="#">ベーコンバーガー</a></li>
+                        <li><a href="#">チキンバーガー</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">サイド</a>
+                    <ul class="c-navMenu">
+                        <li><a href="#">ポテト</a></li>
+                        <li><a href="#">サラダ</a></li>
+                        <li><a href="#">ナゲット</a></li>
+                        <li><a href="#">コーン</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">ドリンク</a>
+                    <ul class="c-navMenu">
+                        <li><a href="#">コーラ</a></li>
+                        <li><a href="#">ファンタ</a></li>
+                        <li><a href="#">オレンジ</a></li>
+                        <li><a href="#">アップル</a></li>
+                        <li><a href="#">紅茶（Ice/Hot）</a></li>
+                        <li><a href="#">コーヒー（Ice/Hot）</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </aside>
+    <footer class="l-footer p-footer">
+        <ul class="p-footer__ul">
+            <li><a href="#">ショップ情報</a></li>
+            <li>&nbsp;&verbar;&nbsp;</li>
+            <li><a href="#">ヒストリー</a></li>
+        </ul>
+        <p class="p-footer__copyright c-copyright">Copyright: RaiseTech</p>
+    </footer>
+    </div>
+</body>
+</html>
