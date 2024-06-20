85cee61794711dcba4aba32f162622574a50f93a
Yosshiii43 <daisy440440@gmail.com>
2024-06-20 14:47:55 +0900

    index.phpからheader.phpとfooter.phpを分割

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer class="l-footer p-footer">
+        <ul class="p-footer__ul">
+            <li><a href="#">ショップ情報</a></li>
+            <li>&nbsp;&verbar;&nbsp;</li>
+            <li><a href="#">ヒストリー</a></li>
+        </ul>
+        <p class="p-footer__copyright c-copyright">Copyright: RaiseTech</p>
+    </footer>
+    </div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@100;300;400;500;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">    <link rel="stylesheet" href="./css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script type="text/javascript" src="./js/main.js"></script>
+
+</head>
+<body class="p-index c-body">
+    <div class="wrapper">
+    <header class="l-header p-header">
+        <button class="p-header__btn c-title js-navButton"><span>Menu</span></button>
+        <p class="p-header__ttl c-title"><a href="#">Hamburger</a></p>
+        <div class="p-header__search">
+            <form action="" >
+                <div class="c-searchBox">
+                    <input type="search">
+                    <p class="c-seachIcon"><img src="./img/search__icon.png" alt="ルーペアイコン" width="24px" height="24px"></p>
+                </div>
+                <button class="c-button" type="button" >検索</button>
+            </form>
+        </div>
+    </header>
