4dbef295d690793448d38167ef8f9debff545a74
Yosshiii43 <daisy440440@gmail.com>
2024-06-23 02:48:52 +0900

    title出力設定

--- functions.php
+++ functions.php
@@ -1,9 +1,26 @@
 <?php
 function add_files() {
     //読み込みたいファイルを書く
-    wp_enqueue_style('hamburger-style', get_theme_file_uri('/css/style.css'),array());
-    wp_enqueue_style('googlefonts','//fonts.googleapis.com/css2?family=M+PLUS+1p:wght@100;300;400;500;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap', array());
+    wp_enqueue_style('hamburger-style', get_theme_file_uri('/css/style.css'),array(),'1.0.0');
+    wp_enqueue_style('googlefonts','//fonts.googleapis.com/css2?family=M+PLUS+1p:wght@100;300;400;500;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap', array(),'1.0.0');
     wp_enqueue_script('jquery');
     wp_enqueue_script('hamburger-js',get_theme_file_uri('/js/main.js'),array('jquery'),'',true);
 }
-add_action( 'wp_enqueue_scripts', 'add_files');
+add_action('wp_enqueue_scripts', 'add_files');
+
+function theme_setup(){
+    //テーマサポート
+    add_theme_support('title-tag');
+}
+add_action('after_setup_theme', 'theme_setup');
+
+//タイトル出力
+function hamburger_title($title){
+    if(is_front_page() && is_home()){//トップページなら
+        $title = get_bloginfo('name','display');
+    } elseif(is_singular()){//シングルページなら
+        $title = single_post_title('', false);
+    }
+    return $title;
+}
+add_filter('pre_get_document_title','hamburger_title');
--- header.php
+++ header.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" contentt="<?php bloginfo( 'description' ); ?>">
-    <title>Document</title>
+    <meta name="description" contentt="<?php bloginfo('description'); ?>">
     <?php wp_head(); ?>
 </head>
-<body class="<?php body_class( 'c-body' ); ?> ">
+<body class="<?php body_class('c-body'); ?> ">
     <?php wp_body_open(); ?>
     <div class="wrapper">
     <header class="l-header p-header">
