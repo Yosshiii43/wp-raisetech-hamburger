1172102b8abf3437deb4e9368f96c1cb75fbd3fb
Yosshiii43 <daisy440440@gmail.com>
2024-07-02 02:58:07 +0900

    sidebar.phpから元のメニューコード削除

--- sidebar.php
+++ sidebar.php
@@ -10,39 +10,6 @@
                 )
             );
             ?>
-            <!--
-            <ul class="p-gnav c-gnav">
-                <li ><a href="#">バーガー</a>
-                    <ul class="c-gnav">
-                        <li><a href="#">ハンバーガー</a></li>
-                        <li><a href="#">チーズバーガー</a></li>
-                        <li><a href="#">テリヤキバーガー</a></li>
-                        <li><a href="#">アボガドバーガー</a></li>
-                        <li><a href="#">フィッシュバーガー</a></li>
-                        <li><a href="#">ベーコンバーガー</a></li>
-                        <li><a href="#">チキンバーガー</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">サイド</a>
-                    <ul class="c-navMenu">
-                        <li><a href="#">ポテト</a></li>
-                        <li><a href="#">サラダ</a></li>
-                        <li><a href="#">ナゲット</a></li>
-                        <li><a href="#">コーン</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">ドリンク</a>
-                    <ul class="c-navMenu">
-                        <li><a href="#">コーラ</a></li>
-                        <li><a href="#">ファンタ</a></li>
-                        <li><a href="#">オレンジ</a></li>
-                        <li><a href="#">アップル</a></li>
-                        <li><a href="#">紅茶（Ice/Hot）</a></li>
-                        <li><a href="#">コーヒー（Ice/Hot）</a></li>
-                    </ul>
-                </li>
-            </ul>
--->
         </nav>
         <div class="p-navbg"></div>
     </aside>
