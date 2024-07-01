bd4a861981a4df7d2eef67cd0338ef4efdf20b74
Yosshiii43 <daisy440440@gmail.com>
2024-07-02 02:50:13 +0900

    メインメニュー設置

--- footer.php
+++ footer.php
@@ -3,6 +3,8 @@
             wp_nav_menu(
                 array(
                     'thema_location' => 'footer_nav',
+                    'menu_class' => 'p-footer__ul',//静的サイトと同じclassを付与
+                    'container' => 'false',//divタグで囲まない
                 )
             );
             ?>
--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,16 @@
 <aside class="l-sidebar p-sidebar">
         <nav class="p-nav">
             <h2 class="c-title">Menu</h2>
+            <?php
+            wp_nav_menu(
+                array(
+                    'theme_location' => 'main_nav',
+                    'menu_class' => 'p-gnav',//静的サイトと同じclassを付与
+                    'container' => 'false',//divタグで囲まない
+                )
+            );
+            ?>
+            <!--
             <ul class="p-gnav c-gnav">
                 <li ><a href="#">バーガー</a>
                     <ul class="c-gnav">
@@ -32,6 +42,7 @@
                     </ul>
                 </li>
             </ul>
+-->
         </nav>
         <div class="p-navbg"></div>
     </aside>
