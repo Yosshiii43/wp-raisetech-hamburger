7474139373e696bc3490dc7be84e12231efdbb93
Yosshiii43 <daisy440440@gmail.com>
2024-07-11 03:25:59 +0900

    404ページ修正

--- 404.php
+++ 404.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
     <main class="l-main">
         <div class="p-wrapper p-wrapper--404">
-            <p class="u-marginBottom--10">お探しのページは見つかりませんでした</p>
+            <p class="u-marginBottom--10">お探しのページは見つかりませんでした。</p>
             <p><a class="u-underLine" href="<?php echo esc_url(home_url('/')); ?>">トップページへ戻る</a></p>
         </div>
     </main>
