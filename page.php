193690e38334aefeee99f2fedc6ed281e2bab2d8
Yosshiii43 <daisy440440@gmail.com>
2024-07-18 21:15:50 +0900

    検索フォーム修正・固定ページサブタイトル追加

--- page.php
+++ page.php
@@ -6,11 +6,15 @@
                 the_post();
                 ?>
         <div id="post-<?php the_ID(); ?>" <?php post_class( 'p-wrapper' ); ?>>
+            <?php $h2value = get_post_meta($post->ID,'sub_title',true); ?>
+            <?php if(!empty( $h2value )): ?>
+                <h2 class="c-hedding--h2 u-marginBottom--60"><?php the_field('sub_title'); ?></h2>
+            <?php endif; ?>
                 <?php the_content(); ?>
                 <?php wp_link_pages(); ?>
         </div><!--p-wrapper-->
         <?php endwhile; else :?>
-            <p>表示する記事がありません</p>
+            <p>表示する記事がありません</p> 
         <?php endif; ?>
     </main>
     <?php get_sidebar(); ?>
--- searchform.php
+++ searchform.php
@@ -2,7 +2,7 @@
         <form id="searchform" method="get" action="<?php echo esc_url(home_url('/')); ?>">
             <div class="c-searchBox">
                 <input type="search" class="search-field"  value="" name="s" />
-                <p class="c-seachIcon"><img src="<?php echo esc_url(get_theme_file_uri()); ?>/img/search__icon.png" alt="ルーペアイコン" width="24px" height="24px"></p>
+                <p class="c-searchIcon"><img src="<?php echo esc_url(get_theme_file_uri()); ?>/img/search__icon.png" alt="ルーペアイコン" width="24px" height="24px"></p>
             </div>
             <input type="submit" class="c-button search-submit" value="検索" />
         </form>
